80fb54c5969a35f282f04c5df0e58765d97717bf
roman.ali <roman.ali@spectrum-bd.com>
2025-09-17 17:48:42 +0600

    update directory

--- pharma-pro-app/src/app/app.routes.ts
+++ pharma-pro-app/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/user/components/login/login.component';
 import { RegisterComponent } from './modules/user/components/register/register.component';
 import { UsersComponent } from './modules/user/components/users/users.component';
-import { DashboardComponent } from './modules/dashboard/components/dashboard/dashboard.component';
+import { DashboardComponent } from './modules/dashboard/component/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
